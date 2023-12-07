5ee2a12dadf1b6ad4ab7e662339b5a008d71f72e
smhavens <smhavens@umich.edu>
2023-12-06 23:07:52 -0500

    InputExample Attempt again

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def training():
     for i in range(n_examples):
         example = train_data[i]
         # print(example)
-        train_examples.append(InputExample(texts=[example['text']], label =[example['label']]))
+        train_examples.append(InputExample(texts=example['text'], label=example['label']))
         
     train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=25)
     
