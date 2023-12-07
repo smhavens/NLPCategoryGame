14a77f6d2f32537061bfee09b1266081906db965
smhavens <smhavens@umich.edu>
2023-12-06 22:44:19 -0500

    remove label, please work

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def training():
     for i in range(n_examples):
         example = train_data[i]
         # print(example)
-        train_examples.append(InputExample(texts=[example['text'], example['label']]))
+        train_examples.append(InputExample(texts=[example['text']]))
         
     train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=25)
     
